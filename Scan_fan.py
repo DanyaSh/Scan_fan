78ab7a04e989d04aac7b95e48618ca9741684c8a
Danya_dell_linux <danilashundeev@gmail.com>
2021-02-17 16:24:42 +0500

    do not work

--- Scan_fan.py
+++ Scan_fan.py
@@ -1,10 +1,25 @@
     # ********************___01_first_00_draft___************************
 '''
-    Что нового:
-    🟢    1)  Создать проект
-    🟡    2)  Написать код
-    🔴    3)  Сделать невозможное
+Legend:
+🟢  1)  Создать проект
+🟡  2)  Найти на сайте ссылку и скачать файл
+🔴  3)  Сделать невозможное
 
 '''
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 
+UserAgent().chrome
 
+# url = 'https://quotes.toscrape.com/'
+url = 'https://2ip.ru/'
+# url = 'https://www1.thepiratebay3.to/top/all'
+response = requests.get(url, headers={'User-Agent': UserAgent().chrome})
+# response = requests.get(url)
+soup = BeautifulSoup(response.text, 'lxml')
+
+# print(soup)
+file=open(("test.html"),"w")
+file.write(str(soup))
+file.close()

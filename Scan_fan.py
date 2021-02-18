1894868556012004b18bdd5f4f2bb4ce3261530e
Danya_dell_linux <danilashundeev@gmail.com>
2021-02-18 09:57:13 +0500

    new

--- Scan_fan.py
+++ Scan_fan.py
@@ -6,21 +6,15 @@ Legend:
 🔴  3)  Сделать невозможное
 
 '''
-from __future__ import division
-from __future__ import absolute_import
-from __future__ import print_function
-from __future__ import unicode_literals
-from selenium import webdriver
-from lxml import html
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--user-data-dir=D:\\project\\User Data") # директория сохранения профиля браузера
-dcap = dict(DesiredCapabilities.CHROME)
-chrome = webdriver.Remote(command_executor='http://127.0.0.1:9515', desired_capabilities=dcap, options=chrome_options)
-chrome.get('https://какой-то сайт/')
-tree = html.fromstring(chrome.page_source)
-link_l = [] #список ссылок на странице
-list_group = tree.xpath('//a') # выбираем все ссылки на странице с помощью Xpath
-for lg in list_group:
-    link = lg.xpath('./@href') #получаем ссылку
-    link_l.append(link) # добавляем ссылку в список
-print (link_l) 
+from selenium.webdriver import Firefox
+import pandas as pd
+
+webdriver = "geckodriver"
+
+driver = Firefox(webdriver)
+
+url = 'https://quotes.toscrape.com/'
+# url = 'https://2ip.ru/'
+# url = 'https://www1.thepiratebay3.to/top/all'
+
+driver.get(url)
